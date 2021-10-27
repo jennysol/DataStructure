8aea5fcb7dbd40649279b340202f482fc30e70bf
jennysol <jennyssoliver@gmail.com>
2021-10-26 22:10:34 -0300

    Feature: Add queue

---
+++ SortingAlgorithms/queue.php
@@ -0,0 +1,22 @@
+<?php
+
+// $lista = array();
+
+// $lista[]=1;
+// $lista[]=2;
+
+// print_r($lista);
+
+$fila = array();
+
+array_push($fila, 'maria','joao','jose');
+
+print_r($fila);
+
+array_shift($fila);
+
+print_r($fila);
+
+array_push($fila, 'outra maria');
+
+print_r($fila);
